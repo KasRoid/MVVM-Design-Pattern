13042fd4275f0459987f21f82a44beef9d2a904a
KasRoid <KasRoid@gmail.com>
2021-02-12 17:58:50 +0900

    init: Created GoodWeather Project and Setup Layouts

---
+++ GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by Kas Song on 2/12/21.
+//
+
+import UIKit
+
+class AddWeatherCityViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+}
+
+// MARK: - UI
+extension AddWeatherCityViewController {
+    final private func setUI() {
+        setBasics()
+    }
+    final private func setBasics() {
+        
+    }
+}
---
+++ GoodWeather/GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Kas Song on 2/12/21.
+//
+
+import UIKit
+
+class WeatherListTableViewController: UITableViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+}
+
+// MARK: - UI
+extension WeatherListTableViewController {
+    final private func setUI() {
+        setBasics()
+    }
+    final private func setBasics() {
+        
+    }
+}
